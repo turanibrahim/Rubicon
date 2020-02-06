09347235db057e9d678b615ece760e2abfe87a07
İbrahim TURAN <ibrahimturan002@gmail.com>
2020-02-06 23:08:36 +0300

    initial commit(haydi bismillah)

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+// dependency'ler ekleniyor
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express() // express app yaratılıyor// dependency'ler kullanılıyor
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors())// client 8080 server 8081'de çalışacak
+
+app.get('/todo', (req, res) => {
+    res.send([
+     'Yapılacak 1',
+     'Yapılacak 2'
+    ])
+})
+
+app.listen( process.env.PORT || 8081)
