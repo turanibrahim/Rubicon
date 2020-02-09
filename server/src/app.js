a060840aad3492cfcc1e6426b2e4205090a84026
İbrahim TURAN <ibrahimturan002@gmail.com>
2020-02-09 15:21:58 +0300

    made db connection and added .env module on project

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,26 @@
 // dependency'ler ekleniyor
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
-const app = express() // express app yaratılıyor// dependency'ler kullanılıyor
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const db = require('mongoose');
+const dotenv = require('dotenv');
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cors())// client 8080 server 8081'de çalışacak
+//App Settings
+const app = express(); // creating Express app
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors()); // client working on 8080,  server working on 8081
+dotenv.config({ path: '../.env' });
+
+//DB Settings
+const CONNECTION_URL = process.env.DB;
+db.connect(CONNECTION_URL, {useNewUrlParser: true ,useUnifiedTopology: true}, (err) => {
+    if(err)
+        console.log(err);
+    else
+        console.log('MongoDB connected successfully.');
+});
 
 app.get('/todo', (req, res) => {
     res.send([
