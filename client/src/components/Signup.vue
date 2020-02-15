15413dce4e84402c47f85df40d14bbff26e24fb2
İbrahim TURAN <ibrahimturan002@gmail.com>
2020-02-16 00:27:51 +0300

    continued to signup page design and added func to pass between auth components

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -1,37 +1,36 @@
 <template>
   <v-row class="custom-row">
-    <v-col cols="6" class="py-0 d-flex align-stretch">
-      <v-img
-        height="100vh"
-        src="https://images.pexels.com/photos/1227520/pexels-photo-1227520.jpeg"
-      ></v-img>
-    </v-col>
     <v-col cols="6" justify-center class="align-self-center" >
       <v-row justify="center">
         <v-col cols="8">
           <v-card shaped elevation="24" class="pa-2">
-            <v-img
-              class="white--text align-end mt-2"
-              contain
-              height="200px"
-              src="https://lh3.googleusercontent.com/Hpk4d4ezPJjMw7F2polb7jvRamJEGLJVNKJFg_GaYH9lS7i_GkTqsdLzLZBWqyBZuSc4=s180-rw"
-            >
-            </v-img>
-            <v-card-title class="justify-center text-uppercase py-0 primary--text">
-              login
+            <v-card-title class="justify-center text-uppercase  primary--text">
+              sign up
             </v-card-title>
             <v-card-text>
                 <template>
                   <v-container py-0>
                     <v-row justify="center">
-                      <v-col cols="12" xs="12" class="align-self-center text-center">
-                        <v-alert
-                          border="left"
-                          color="red"
-                          v-if="this.errorMessage"
-                        >
-                          Error! Email or password is wrong.
-                        </v-alert>
+                      <v-col cols="10" xs="12">
+                        <v-text-field
+                          label="Name"
+                          outlined
+                          hide-details
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="10" xs="12">
+                        <v-text-field
+                          label="Surname"
+                          outlined
+                          hide-details
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="10" xs="12">
+                        <v-text-field
+                          label="Username"
+                          outlined
+                          hide-details
+                        ></v-text-field>
                       </v-col>
                       <v-col cols="10" xs="12">
                         <v-text-field
@@ -60,8 +59,8 @@
                           color="blue lighten-4"
                         >
                           <span>
-                            Do you want to sign up?
-                            <a text small @click="goToSignup()">Click here.</a>
+                            Do you have an account ?
+                            <a text small @click="goToLogin()">Click here.</a>
                           </span>
                         </v-alert>
                       </v-col>
@@ -69,7 +68,7 @@
                         <v-btn
                           block
                           color="primary"
-                        >Login</v-btn>
+                        >Signup</v-btn>
                       </v-col>
                     </v-row>
                   </v-container>
@@ -78,19 +77,25 @@
         </v-col>
       </v-row>
     </v-col>
+    <v-col cols="6" class="py-0 d-flex align-stretch">
+      <v-img
+        height="100vh"
+        src="https://images.pexels.com/photos/1227520/pexels-photo-1227520.jpeg"
+      ></v-img>
+    </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: 'Login',
+  name: 'SignUp',
   data: () => ({
     errorMessage: false,
   }),
   methods: {
     // eslint-disable-next-line no-unused-vars
-    goToSignup(event) {
-      const value = 'changeScreenToSignup';
+    goToLogin(event) {
+      const value = 'changeScreenToLogin';
       this.$emit(value);
     },
   },
--- client/src/views/Auth.vue
+++ client/src/views/Auth.vue
@@ -1,16 +1,26 @@
 <template>
-  <login></login>
+  <v-fade-transition>
+    <login v-if="whichScreen" @changeScreenToSignup="changeScreen()"></login>
+    <signup v-else @changeScreenToLogin="changeScreen()"></signup>
+  </v-fade-transition>
 </template>
 <script>
 import Login from '../components/Login.vue';
+import Signup from '../components/Signup.vue';
 
 export default {
   name: 'Auth',
   components: {
     Login,
+    Signup,
   },
   data: () => ({
-  //
+    whichScreen: true,
   }),
+  methods: {
+    changeScreen() {
+      this.whichScreen = !this.whichScreen;
+    },
+  },
 };
 </script>
