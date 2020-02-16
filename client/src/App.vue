3e70d3e5304d903b432e153456e4558fc52ef683
Sehit Samil Gokmen <samil-98@hotmail.com>
2020-02-16 21:04:52 +0300

    component files added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <v-app>
+    <navbar/>
    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
+  components: {
+    Navbar,
+  },
 
   data: () => ({
     //
