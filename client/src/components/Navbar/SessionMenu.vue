4257d9208d58e3a9e8b4691272c8fba9c52c8d04
Sehit Samil Gokmen <samil-98@hotmail.com>
2020-02-20 01:46:22 +0300

    new component for navbar added

---
+++ client/src/components/Navbar/SessionMenu.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name:'SessionMenu',
+  data () {
+    return {
+      items: [ 'Login', 'Sign-Up'],
+    };
+  },
+};
+</script>
+
+<template>
+<v-row>
+  <v-col
+    md1
+    v-for="(item, index) in items" 
+    :key="index" 
+  >
+    <v-btn 
+      color="blue lighten-2"
+    >
+      {{item}}
+    </v-btn>
+  </v-col>
+</v-row>
+</template>
+
+
+<style>
+
+</style>
